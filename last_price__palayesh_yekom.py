4e39df26d19ea04a64cf584ba2adce25b1a32575
sadegh-dev <sadegh.rez@gmail.com>
2022-05-11 11:54:45 +0430

    #last_price_palayesh_yekom set webdriver

--- last_price__palayesh_yekom.py
+++ last_price__palayesh_yekom.py
@@ -4,6 +4,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
-
-
 #time.sleep(15)
+
+PATH = "/usr/bin/chromedriver"
+
+driver = webdriver.Chrome(PATH)
+
+driver.get("http://www.tsetmc.com/Loader.aspx?ParTree=151311&i=67675656072510693")
+
+
