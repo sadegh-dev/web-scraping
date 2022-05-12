6f3be7b9c3cd5bd51a0b4c2ea2f17beb015dc79f
sadegh-dev <sadegh.rez@gmail.com>
2022-05-12 17:12:14 +0430

    #search_and_result.py -complete

---
+++ search_and_return_result.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+PATH = "/usr/bin/chromedriver"
+driver = webdriver.Chrome(PATH)
+
+driver.get("http://boursepress.ir/")
+#print(driver.title)
+
+
+# Search
+
+search = driver.find_element(by=By.NAME, value="q")
+search.send_keys("پالایش یکم")
+search.send_keys(Keys.RETURN)
+
+
+try:
+    content = WebDriverWait(driver, 20).until(
+        EC.presence_of_all_elements_located((By.ID,"ucSearch_divContent"))
+    ) 
+
+    # Read titles
+
+    articles = driver.find_elements(by=By.TAG_NAME, value="li")
+    for article in articles :
+        header = article.find_element(by=By.TAG_NAME, value= "a")
+        print(header.text)
+
+except:
+    print('Error! , Element Not Load')
+    driver.quit()
+
+finally:
+    driver.quit()
+
